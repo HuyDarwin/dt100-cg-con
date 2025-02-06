bd4bda76caaf627f13c87cad0ce912d708e036c9
Glitch (dt100-cg-con) <none>
2025-02-06 13:35:13 +0000

    📚🍲 Checkpoint ./JS/index_fb.js:39200795/3480

--- JS/index_fb.js
+++ JS/index_fb.js
@@ -2,18 +2,33 @@ $.keyframe.define([
   {
     name: 'key-q-reveal',
     '0%': {
-      "transform": "translateY(100px)"
+      "opacity": "1",
+      "transform": "translateY(100%)"
     },
     '100%': {
-       "transform": "translateY(0px)"
+      "opacity": "1",
+       "transform": "translateY(0%)"
+    }
+  },
+  {
+    name: 'key-q-hide',
+    '0%': {
+      "opacity": "1",
+      "transform": "translateY(0%)"
+    },
+    '100%': {
+      "opacity": "1",
+       "transform": "translateY(100%)"
     }
   },
   {
     name: 'key-q-reveal-2',
     '0%': {
+      "opacity": "1",
       "clip-path": "inset(0% 0% 50% 0%)"
     },
     '100%': {
+      "opacity": "1",
        "clip-path": "inset(0% 0% 0% 0%)"
     }
   }
@@ -24,8 +39,7 @@ function ResetQuestion(){
 }
 
 function QuestionReveal() {
-    $('.q-con, ')
-    $('.q-con div').playKeyframe({
+    $('.question').playKeyframe({
       name: 'key-q-reveal',
       duration: '0.75s',
       timingFunction: 'ease'
@@ -37,10 +51,55 @@ function QuestionReveal() {
     })
 }
 
+function RemainReveal() {
+    $('.q-remain').playKeyframe({
+      name: 'key-q-reveal',
+      duration: '0.75s',
+      timingFunction: 'ease'
+    })
+}
+
+function RemainHide() {
+    $('.q-remain').playKeyframe({
+      name: 'key-q-hide',
+      duration: '0.5s',
+      timingFunction: 'ease'
+    })
+}
+
+function MoneyReveal() {
+    $('.q-money').playKeyframe({
+      name: 'key-q-reveal',
+      duration: '0.5s',
+      timingFunction: 'ease'
+    })
+}
+
+function EscapeReveal(){
+    $('.q-escape-p').css('opacity',0)
+  
+    setTimeout(function(){
+      $('#q-escape-p-25').css('opacity',1)
+    }, 100)
+    setTimeout(function(){
+      $('#q-escape-p-50').css('opacity',1)
+    }, 30)
+    setTimeout(function(){
+      $('#q-escape-p-75').css('opacity',1)
+    }, 500)
+  
+    $('.q-escape').playKeyframe({
+      name: 'key-q-reveal',
+      duration: '0.5s',
+      timingFunction: 'linear'
+    })
+}
+
 ResetQuestion()
+QuestionReveal()
 setTimeout(function(){
-  QuestionReveal()
-}, 1000)
+  EscapeReveal()
+}, 2000)
 
 function AnswerStatus (ans, status) {
   var answer = '';
