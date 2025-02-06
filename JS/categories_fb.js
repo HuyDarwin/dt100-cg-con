04dae7ee30e05d3a35af4f321dd060ed559d9e83
Glitch (dt100-cg-con) <none>
2025-02-06 14:57:30 +0000

    🍏📍 Checkpoint ./CSS/site_categories.css:39200795/711 ./categories-atsi.html:39200795/146 ./JS/categories_fb.js:39200795/581

--- JS/categories_fb.js
+++ JS/categories_fb.js
@@ -1,3 +1,19 @@
+function ResetAllTypes(){
+  $('.type').css('top','100%');
+  
+  $('.type-1 #easy').css('left', '-0.25%');
+  $('.type-1 #hard').css('left', '49.75%');
+}
 
+function RevealType (type) {
+  $('.type').css('top','100%');
+  if(type==1){
+    $('#type-1').animate({"top":"0%"},500);
+  }
+}
 
 /* Init */
+ResetAllTypes();
+setTimeout(function(){
+  RevealType(1)
+}, 500)
