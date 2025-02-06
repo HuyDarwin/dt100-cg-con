dfd270ad8f5883bd9bcbfc1bcda751208b86b1cb
Glitch (dt100-cg-con) <none>
2025-02-06 15:09:30 +0000

    🍩🐌 Checkpoint ./JS/categories_fb.js:39200795/5942

--- JS/categories_fb.js
+++ JS/categories_fb.js
@@ -1,14 +1,43 @@
+$.keyframe.define([
+  {
+    name: 'hide',
+    '0%': {
+      "opacity": "1",
+      "scale": "1"
+    },
+    '100%': {
+      "opacity": "0",
+      "scale": "0"
+    }
+  }
+]);
+
 function ResetAllTypes(){
   $('.type').css('top','100%');
   
-  $('.type-1 #easy').css('left', '-0.25%');
-  $('.type-1 #hard').css('left', '49.75%');
+  $('.type-1 #easy').animate({"left":"-0.25%"}, 0);
+  $('.type-1 #easy').resetKeyframe();
+  $('.type-1 #hard').animate({"left":"49.75%"}, 0);
+  $('.type-1 #hard').resetKeyframe();
 }
 
 function RevealType (type) {
   $('.type').css('top','100%');
   if(type==1){
-    $('#type-1').animate({"top":"0%"},500);
+    $('#type-1').animate({"top":"0%"}, 500);
+  }
+}
+
+function ChooseOption (type, opt){
+  if(type==1){
+    if(opt==1){
+      $('#type-1 #hard').playKeyframe({
+        name: 'hide',
+        duration: '0.25s',
+        timingFunction: 'ease'
+      })
+      $('#type-1 #easy').animate({"left":"25%"}, 250);
+    }
   }
 }
 
@@ -17,3 +46,16 @@ ResetAllTypes();
 setTimeout(function(){
   RevealType(1)
 }, 500)
+setTimeout(function(){
+  ChooseOption(1,1)
+}, 2000)
+
+setTimeout(function(){
+  ResetAllTypes();
+}, 3500)
+setTimeout(function(){
+  RevealType(1)
+}, 4500)
+setTimeout(function(){
+  ChooseOption(1,1)
+}, 6000)
