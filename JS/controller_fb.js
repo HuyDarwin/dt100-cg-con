1693a4558175fef0dcf25c65d9b9d65dd452efc8
Glitch (dt100-cg-con) <none>
2025-02-09 08:59:09 +0000

    🐈💳 Checkpoint ./JS/controller_fb.js:39200795/3399

--- JS/controller_fb.js
+++ JS/controller_fb.js
@@ -12,6 +12,9 @@ onValue(ref(db), (snapshot) => {
 
 //
 
+var MAX_QS = 50;
+
+var questions = [];
 
 //
 
@@ -19,3 +22,34 @@ remove(ref(db));
 $('button').attr("disabled","true");
 $('.reload').removeAttr("disabled");
 $('.q-submit').removeAttr("disabled");
+$('.c-reveal').removeAttr("disabled");
+
+$('.get-qs').on("change", function(e){
+  var file = e.target.files[0];
+  var reader = new FileReader();
+  reader.onload = function(e) {
+    var data = e.target.result;
+    var workbook = XLSX.read(e.target.result);
+    var sheet = workbook.Sheets[workbook.SheetNames[0]];
+
+    questions = [];
+    
+    for(var i = 1; i <= MAX_QS; i++) {
+        var last = 3 + (i - 1) * 11;
+        questions.push({
+          Difficulty: sheet['C' + i].v,
+          Question: sheet['B' + i].v.replace("++++", "<br />"),
+          AnswerA: sheet['C' + i].v,
+          AnswerB: sheet['D' + i].v,
+          AnswerC: sheet['E' + i].v,
+          AnswerD: sheet['F' + i].v,
+          AnsOrder1: sheet['G' + i].v,
+          AnsOrder2: sheet['H' + i].v,
+          AnsOrder3: sheet['I' + i].v,
+          AnsOrder4: sheet['J' + i].v,
+          Note: sheet['K' + i].v
+        })
+    }
+  };
+  reader.readAsArrayBuffer(file);
+})
