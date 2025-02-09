e171e66317750c0ee2d543b3fd811cf64a67cc7b
Glitch (dt100-cg-con) <none>
2025-02-09 09:10:31 +0000

    🍭🐛 Checkpoint ./JS/controller_fb.js:39200795/3896

--- JS/controller_fb.js
+++ JS/controller_fb.js
@@ -16,6 +16,8 @@ var MAX_QS = 50;
 
 var questions = [];
 
+var question_now = 0;
+
 //
 
 remove(ref(db));
@@ -37,19 +39,61 @@ $('.get-qs').on("change", function(e){
     for(var i = 1; i <= MAX_QS; i++) {
         var last = 3 + (i - 1) * 11;
         questions.push({
-          Difficulty: sheet['C' + i].v,
-          Question: sheet['B' + i].v.replace("++++", "<br />"),
-          AnswerA: sheet['C' + i].v,
-          AnswerB: sheet['D' + i].v,
-          AnswerC: sheet['E' + i].v,
-          AnswerD: sheet['F' + i].v,
-          AnsOrder1: sheet['G' + i].v,
-          AnsOrder2: sheet['H' + i].v,
-          AnsOrder3: sheet['I' + i].v,
-          AnsOrder4: sheet['J' + i].v,
-          Note: sheet['K' + i].v
+          Type: sheet['B' + (last + 1)].v,
+          
+          One_Name: sheet['B' + (last + 2)].v,
+          One_Img: sheet['B' + (last + 3)].v,
+          One_Audio: sheet['B' + (last + 4)].v,
+          One_Difficulty: sheet['B' + (last + 5)].v,
+          One_Question: sheet['B' + (last + 6)].v,
+          One_AnswerA: sheet['B' + (last + 7)].v,
+          One_AnswerB: sheet['B' + (last + 8)].v,
+          One_AnswerC: sheet['B' + (last + 9)].v,
+          One_CorrectAns: sheet['B' + (last + 10)].v,
+          One_Note: sheet['B' + (last + 11)].v,
+          
+          Two_Name: sheet['D' + (last + 2)].v,
+          Two_Img: sheet['D' + (last + 3)].v,
+          Two_Audio: sheet['D' + (last + 4)].v,
+          Two_Difficulty: sheet['D' + (last + 5)].v,
+          Two_Question: sheet['D' + (last + 6)].v,
+          Two_AnswerA: sheet['D' + (last + 7)].v,
+          Two_AnswerB: sheet['D' + (last + 8)].v,
+          Two_AnswerC: sheet['D' + (last + 9)].v,
+          Two_CorrectAns: sheet['D' + (last + 10)].v,
+          Two_Note: sheet['D' + (last + 11)].v,
         })
     }
   };
+  
+  $(".list_qs").empty();
+  
+  for(var i = 0; i < questions.length; i++){
+    var type = questions[i].Type;
+    var text = "";
+    if (type == 1) {
+      text = "Khó dễ";
+    }
+    else if (type == 2) {
+      text = "Khó dễ có hình ảnh";
+    }
+    else if (type == 3) {
+      text = "2 chủ đề";
+    }
+    else if (type == 4) {
+      text = "2 chủ đề có hình ảnh";
+    }
+    else if (type == 5) {
+      text = "2 hình ảnh";
+    }
+    else if (type == 6) {
+      text = "2 hình ảnh đính kèm chủ đề";
+    }
+    $(".list_qs").append('<option value="' + (i + 1) + '">' + (i + 1) + '. ' + text + '</option>');
+    
+    question_now = 1;
+    
+  }
+  
   reader.readAsArrayBuffer(file);
 })
