616e1e8b66c6dc2ce195b0f90385c45854ca8c58
Glitch (dt100-cg-con) <none>
2025-02-09 08:35:37 +0000

    😎🎅 Checkpoint ./controller-dama.html:39200795/184 ./JS/controller_fb.js:39200795/14973 ./CSS/site_controller.css:39200795/221

---
+++ JS/controller_fb.js
@@ -0,0 +1,19 @@
+import { getDatabase, ref, set, update, onValue, remove, get } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-database.js";
+//
+
+const db = getDatabase();
+
+//
+
+onValue(ref(db), (snapshot) => {
+    const data = snapshot.val();
+
+});
+
+//
+
+
+//
+
+remove(ref(db));
+$('button').attr("disabled","true");
