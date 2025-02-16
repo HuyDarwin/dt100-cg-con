69deb43bd2bfac4bd067c2de729d2c20a2b9a7d3
Glitch (dt100-cg-con) <none>
2025-02-16 11:45:00 +0000

    🎖🍔 Checkpoint ./JS/host_fb.js:39200795/2271 ./controller-dama.html:39200795/1487 ./CSS/site_controller.css:39200795/311

--- JS/host_fb.js
+++ JS/host_fb.js
@@ -39,6 +39,48 @@ $(function () {
               upd("reload", 0);
             }
           
+            if(data.q_type == 1) {
+              $(".type").html("Loại câu hỏi: Khó dễ");
+            }
+            else if(data.q_type == 2) {
+              $(".type").html("Loại câu hỏi: Khó dễ có hình ảnh");
+            }
+            else if(data.q_type == 3) {
+              $(".type").html("Loại câu hỏi: 2 chủ đề");
+            }
+            else if(data.q_type == 4) {
+              $(".type").html("Loại câu hỏi: 2 chủ đề có hình ảnh");
+            }
+            else if(data.q_type == 5) {
+              $(".type").html("Loại câu hỏi: 2 hình ảnh");
+            }
+            else if(data.q_type == 6) {
+              $(".type").html("Loại câu hỏi: 2 hình ảnh đính kèm chủ đề");
+            }
+            else {
+              $(".type").html("Loại câu hỏi: ");
+            }
+          
+            if(data.correct_ans == 1) {
+              $(".right-ans").html("Đáp án đúng: A");
+            }  
+            else if(data.correct_ans == 2) {
+              $(".right-ans").html("Đáp án đúng: B");
+            }   
+            else if(data.correct_ans == 3) {
+              $(".right-ans").html("Đáp án đúng: C");
+            }       
+            else {
+              $(".right-ans").html("Đáp án đúng: ");
+            } 
+          
+            $(".note").html(data.note);
+          
+            $(".mob-remain-p").html(data.mob_left);
+            $(".mob-w-e-p").html(data.mob_to_eli);
+            $(".money-add-p").html(accounting.formatMoney(data.eli_money));
+            $(".money-total-p").html(accounting.formatMoney(data.total_money));
+            $(".mob-e-p").html(data.mob_eli);         
           
         });
       
