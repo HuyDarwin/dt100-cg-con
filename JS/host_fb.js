c1e6d08e0398f48d50f6c0709ed0c9eb3b22a1c7
Glitch (dt100-cg-con) <none>
2025-02-16 11:32:56 +0000

    👓🦄 Checkpoint ./host-tule.html:39200795/4131 ./CSS/site_host.css:39200795/1739 ./JS/host_fb.js:39200795/36615

---
+++ JS/host_fb.js
@@ -0,0 +1,46 @@
+import { getDatabase, ref, set, update, onValue, remove, get } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-database.js";
+
+$(function () {
+    "use strict";
+
+    window.CONTROLLER = window.CONTROLLER || {};
+
+    (function (con) {
+        //
+
+        const db = getDatabase();
+
+        // Variables
+
+        //
+        function upd(key, val) {
+            update(ref(db), {
+                [key]: val
+            })
+        }
+
+        function enb(key) {
+            $(key).removeAttr('disabled')
+        }
+
+        function dib(key) {
+            $(key).attr('disabled', true);
+        }
+
+        
+
+        //
+
+        onValue(ref(db), (snapshot) => {
+            const data = snapshot.val();
+
+            if(data.reload == 1) {
+              location.reload();
+              upd("reload", 0);
+            }
+          
+          
+        });
+      
+    }(window.CONTROLLER = window.CONTROLLER || {}));
+});
