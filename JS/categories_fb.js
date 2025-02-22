b59c37becd03cf406fced1a39581bbdf6884ea2f
Glitch (dt100-cg-con) <none>
2025-02-22 12:38:32 +0000

    🛎🌈 Checkpoint ./JS/screen_fb.js:39200795/48 ./JS/categories_fb.js:39200795/494

--- JS/categories_fb.js
+++ JS/categories_fb.js
@@ -21,11 +21,11 @@ $(function () {
             name: 'hide',
             '0%': {
               "opacity": "1",
-              "scale": "1"
+              "transform": "scale(1)"
             },
             '100%': {
               "opacity": "0",
-              "scale": "0"
+              "transform": "scale(0)"
             }
           },
           {
--- JS/screen_fb.js
+++ JS/screen_fb.js
@@ -145,11 +145,11 @@ $(function () {
             name: 'hide',
             '0%': {
               "opacity": "1",
-              "scale": "1"
+              "transform": "scale(1)"
             },
             '100%': {
               "opacity": "0",
-              "scale": "0"
+              "transform": "scale(0)"
             }
           },
           {
